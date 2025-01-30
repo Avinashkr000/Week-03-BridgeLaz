f0f0bb2ccab2109328f0720914637f9ec34d8674
Avinashkr000 <akum97543@gmail.com>
2025-01-30 10:13:56 +0530

    Added

---
+++ com/linkedlist/singly/studentrecordmanagement/StudentNode.java
@@ -0,0 +1,53 @@
+package com.linkedlist.singly.studentrecordmanagement;
+
+public class StudentNode {
+    private int rollNumber;
+    private String name;
+    private int age;
+    private String grade;
+    StudentNode next;
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public StudentNode getNext(){
+        return next;
+    }
+
+    public StudentNode(int rollNumber, String name, int age, String grade) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+        this.next = null;
+    }
+}
