20e3d6e47fd9ff243807bdbabf6c164f00454c1c
Avinashkr000 <akum97543@gmail.com>
2025-01-31 20:26:42 +0530

    Added

---
+++ bubblesort/BubbleSort.java
@@ -0,0 +1,24 @@
+package com.day03.bubblesort;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int[] marks) {
+        int n = marks.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (marks[j] > marks[j + 1]) {
+                    int temp = marks[j];
+                    marks[j] = marks[j + 1];
+                    marks[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+}
