247716a9afd340fea6c4e00e133d47b4edbe0660
Avinashkr000 <akum97543@gmail.com>
2025-01-31 20:20:52 +0530

    Added

---
+++ stacksandqueus/circulartourproblem/CircularTourProblem.java
@@ -0,0 +1,24 @@
+package com.Day02.stacksandqueus.circulartourproblem;
+
+
+public class CircularTourProblem {
+
+    public static int findStartingPoint(int[] petrol, int[] distance) {
+        int n = petrol.length;
+        int start = 0;
+        int surplus = 0;
+        int deficit = 0;
+
+        for (int i = 0; i < n; i++) {
+            surplus += petrol[i] - distance[i];
+            if (surplus < 0) {
+                deficit += surplus;
+                surplus = 0;
+                start = i + 1;
+            }
+        }
+
+        return (surplus + deficit >= 0) ? start : -1;
+    }
+
+}
