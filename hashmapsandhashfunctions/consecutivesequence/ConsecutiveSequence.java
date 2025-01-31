a341f73c8913b06e7497a6d54c46f8b3aea09dde
Avinashkr000 <akum97543@gmail.com>
2025-01-31 20:14:31 +0530

    Added

---
+++ hashmapsandhashfunctions/consecutivesequence/ConsecutiveSequence.java
@@ -0,0 +1,34 @@
+package com.Day02.hashmapsandhashfunctions.consecutivesequence;
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConsecutiveSequence {
+
+    public static int longestConsecutive(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : numSet) {
+            if (!numSet.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (numSet.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+
+}
