67efc96e57c9e85978503fe5fc8dafb715c8ceb7
Avinashkr000 <akum97543@gmail.com>
2025-02-06 09:54:03 +0530

    [Avinash kumar] linearsearch problem2 solved

---
+++ linearsearch/problem2/LinearSearchWord.java
@@ -0,0 +1,12 @@
+package org.day04.linearsearch.problem2;
+
+public class LinearSearchWord {
+    public static String findSentenceWithWord(String[] sentences, String word) {
+        for (String sentence : sentences) {
+            if (sentence.contains(word)) {
+                return sentence;
+            }
+        }
+        return "Not Found";
+    }
+}
