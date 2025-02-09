44ec6316089c50a827b102e5b0cbe3a9e9ba7f22
Avinashkr000 <akum97543@gmail.com>
2025-02-09 19:55:54 +0530

    [Avinash kumar] problem4 Reading File Solved

---
+++ problem4/Main.java
@@ -0,0 +1,10 @@
+package org.day06.problem4;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String filePath="D:\\CapgTrainingAssignments\\laptop.jpg";
+        ReadingFile readingFile=new ReadingFile();
+        readingFile.compareFileReaderAndInuptStreamReader(filePath);
+    }
+}
