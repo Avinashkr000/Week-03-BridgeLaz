c5bb37a53e0bb1aaea3af2d27b9cd6c0580b2ccb
Avinashkr000 <akum97543@gmail.com>
2025-02-09 19:55:24 +0530

    [Avinash kumar] problem3 StringConcatenate Solved

---
+++ problem3/Main.java
@@ -0,0 +1,14 @@
+package org.day06.problem3;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println(" Enter a string : ");
+        String s=sc.next();
+        StringConcatenate obj=new StringConcatenate();
+        System.out.println(" Time taken in concatenation by String : "+obj.concatenateStrings(s));
+        System.out.println(" Time taken in concatenation by StringBuffer : "+obj.concatenateStringsBuffer(s));
+        System.out.println(" Time taken in concatenation by StringBuilder : "+obj.concatenateStringsBuffer(s));
+    }
+}
---
+++ problem3/StringConcatenate.java
@@ -0,0 +1,36 @@
+package org.day06.problem3;
+
+public class StringConcatenate {
+    public long concatenateStrings(String string) {
+        String concatenatedString = "";
+        long endTime;
+        long startTime = System.nanoTime();
+        for (int i = 0; i < 10000; i++) {
+            concatenatedString += string;
+        }
+        endTime = System.nanoTime();
+        return endTime - startTime;
+    }
+
+    public long concatenateStringsBuffer(String string) {
+        StringBuffer stringBuffer = new StringBuffer("");
+        long endTime;
+        long startTime = System.nanoTime();
+        for (int i = 0; i < 10000; i++) {
+            stringBuffer.append(string);
+        }
+        endTime = System.nanoTime();
+        return endTime - startTime;
+    }
+
+    public long concatenateStringsBuilder(String string) {
+        StringBuilder stringBuilder = new StringBuilder("");
+        long endTime;
+        long startTime = System.nanoTime();
+        for (int i = 0; i < 10000; i++) {
+            stringBuilder.append(string);
+        }
+        endTime = System.nanoTime();
+        return endTime - startTime;
+    }
+}
